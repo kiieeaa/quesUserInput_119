1401c7b3ad95f3d753eb161bb8e04fb2e38875cf
kiieeaa <kiyaadinda5@gmail.com>
2025-10-30 16:24:03 +0700

    Buat deklarasi fungsi composable FormDataDiri

--- app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
@@ -27,3 +27,4 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+fun FormDataDiri(modifier: Modifier) {
