e609c9c971b5638b3d783053e5afaa22a182a378
kiieeaa <kiyaadinda5@gmail.com>
2025-10-30 16:31:57 +0700

    Tambah input field Nama

--- app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
@@ -42,4 +42,15 @@ fun FormDataDiri(modifier: Modifier) {
         verticalArrangement = Arrangement.Top,
         Alignment.CenterHorizontally
     ) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(width = 250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
+    }
     }
