200a01e4b2bda1cbf649a9010aa0e8b4f7727c3a
kiieeaa <kiyaadinda5@gmail.com>
2025-10-30 16:20:05 +0700

    Import material & compose state management

--- app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
@@ -1,4 +1,4 @@
-package com.example.inputpengguna.com.example.praktikum4.ui.theme  
+package com.example.inputpengguna.com.example.praktikum4.ui.theme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import com.example.praktikum4.R
@@ -17,3 +17,12 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
